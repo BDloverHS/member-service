8d94c68e8226a13525c017e038d31162272bc311
BDloverHS <gustn020702naver.com@gmail.com>
2025-01-14 14:39:29 +0900

    memberservice 구축

--- src/main/java/org/config/global/configs/CorsConfig.java
+++ src/main/java/org/config/global/configs/CorsConfig.java
@@ -1,16 +1,20 @@
 package org.config.global.configs;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Configuration
 public class CorsConfig {
-
-    //@Value("${cors.allowed}")
-    //public List<String> allowedOrigin;
+    @Value("${cors.allowed}")
+    public String allowedOrigin;
 
     @Bean
     public CorsFilter corsFilter() {
@@ -21,12 +25,13 @@ public class CorsConfig {
         config.addAllowedMethod("*");
         config.addAllowedHeader("*");
 
-        //if (allowedOrigin == null || allowedOrigin.isEmpty()) {
-            config.addAllowedOrigin("*");
-        //} else {
-        //    config.setAllowedOrigins(allowedOrigin);
+        if (StringUtils.hasText(allowedOrigin)) {
+            List<String> origins = Arrays.stream(allowedOrigin.split(",")).toList();
+            config.setAllowedOrigins(origins);
             config.setAllowCredentials(true);
-        //}
+        } else {
+            config.addAllowedOrigin("*");
+        }
 
         source.registerCorsConfiguration("/**", config);
 
